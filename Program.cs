fbf83aa6293722d4e9ac965324154a8129a6b1fd
Gustavo Rossi Müller <gustavo.rm90@gmail.com>
2025-03-19 09:36:20 -0300

    Exibir algumas informações sobre modelo que está sendo usado.

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ var systemMessage =
 Você é um assistente virtual especializado em responder perguntas frequentes com base em uma lista predefinida. Seu objetivo é fornecer respostas diretas e precisas apenas para as perguntas que estão na lista.
 
 Regras de comportamento:
+- Sempre fale em português do Brasil
 - Se o usuário te cumprimentar, sempre responda com um cumprimento apropriado.
 - Se a pergunta do usuário corresponder exatamente ou for semelhante a uma das perguntas da lista, forneça a resposta correspondente.
 - Se a pergunta não estiver na lista ou não for reconhecida, responda com: "Desculpe, não sei responder a essa pergunta."
@@ -34,16 +35,33 @@ var kernelBuilder = serviceCollection.AddKernel();
 var serviceProvider = serviceCollection.BuildServiceProvider();
 var kernel = serviceProvider.GetRequiredService<Kernel>();
 
-// Pede para baixa o modelo caso necessário
+// Solicita o download do modelo caso necessário
 
 var ollamaClient = serviceProvider.GetRequiredService<OllamaApiClient>();
-var pulling = ollamaClient.PullModelAsync(new OllamaSharp.Models.PullModelRequest() { Model = modelId });
+var pulling = ollamaClient.PullModelAsync(new () { Model = modelId });
 Console.WriteLine($"Preparando modelo {modelId}");
 var emptyLine = new string(' ', Console.BufferWidth) + '\r';
 await foreach (var progress in pulling)
     if (progress != null)
         Console.Write($"{emptyLine}{progress.Status} ({progress.Total:n0} bytes) : {progress.Percent:n2}%\r");
 Console.WriteLine();
+
+// Exibe algumas informações sobre o modelo que está sendo usado
+
+var modelInfo = await ollamaClient.ShowModelAsync(new() { Model = modelId });
+Console.WriteLine("Informações do modelo:");
+Console.WriteLine($"{"Arquitetura",-30}{modelInfo.Info.Architecture}");
+Console.WriteLine($"{"Parâmetros",-30}{modelInfo.Info.ParameterCount:n0}");
+Console.WriteLine($"{"Quantização",-30}{modelInfo.Details.QuantizationLevel}");
+var extraInfo = new Dictionary<string, string>()
+{
+    { "general.languages", "Linguagens" },
+    { "llama.context_length", "Tamanho do Contexto" },
+    { "llama.embedding_length", "Tamanho do Embedding"}
+};
+foreach(var info in extraInfo)
+    if (modelInfo.Info.ExtraInfo?.ContainsKey(info.Key) ?? false)
+        Console.WriteLine($"{info.Value,-30}{modelInfo.Info.ExtraInfo[info.Key]}");
 
 // Adiciona um plugin de FAQ ao kernel
 
